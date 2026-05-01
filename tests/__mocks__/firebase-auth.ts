fe2600089b31421b3aee2942ef480b71fdf0bcb0
SansWord <sansword@gmail.com>
2026-05-01 13:58:42 -0700

    test: add signInAnonymously to firebase-auth mock

--- tests/__mocks__/firebase-auth.ts
+++ tests/__mocks__/firebase-auth.ts
@@ -5,3 +5,4 @@ export class GoogleAuthProvider {}
 export const signInWithPopup = vi.fn()
 export const signOut = vi.fn()
 export const onAuthStateChanged = vi.fn()
+export const signInAnonymously = vi.fn()
